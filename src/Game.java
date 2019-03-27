cde9a22fb96345312cd061373d23e0465aa6d63d
Sabrina <slanza.ieu2015@student.ie.edu>
2019-03-27 22:23:37 +0100

    Difficulty Class UI created

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Game {
 
     Difficulty.Level level;
@@ -10,6 +12,15 @@ public class Game {
         this.numbCells = numbCells;
         this.numbBombs = numbBombs;
 
-        new Board(level);
+        new Board(level, numbCells, numbBombs);
+    }
+
+    public void finishedGame(boolean lostGame) {
+
+        if (lostGame){
+            String textLoser = "GAME OVER";
+            String Announcement = "The game has finished! :(";
+            JOptionPane.showMessageDialog(null, Announcement, textLoser, JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 }
