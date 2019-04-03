2e44529d8fd2c934b2bed1539889a75f14a5ef63
slanzaieu <47603043+slanzaieu@users.noreply.github.com>
2019-04-03 22:23:59 +0200

    week 3

--- src/BombCell.java
+++ src/BombCell.java
@@ -5,11 +5,14 @@ public class BombCell extends Cell {
     public BombCell(Point point) {
 
         super(point);
-        super.setBomb(true);
-        super.setString("x");
-        super.setBackground(Color.RED);
-        super.setOpaque(true);
+        super.setString(" ");
 
     }
+
+    @Override
+    public boolean isBomb() {
+        return true;
+    }
 }
+
 
