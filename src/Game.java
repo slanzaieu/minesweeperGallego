953d50b9a7f3b4b5583e598d1a75f6bf0d1088fe
slanzaieu <47603043+slanzaieu@users.noreply.github.com>
2019-04-03 22:19:26 +0200

    week 3 updated

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.event.WindowEvent;
 
 public class Game {
 
@@ -6,21 +7,34 @@ public class Game {
     int numbCells;
     int numbBombs;
 
-    public Game(Difficulty.Level level, int numbCells, int numbBombs){
+    public Game(Difficulty.Level level, int numbCells, int numbBombs) {
 
         this.level = level;
         this.numbCells = numbCells;
         this.numbBombs = numbBombs;
 
-        new Board(level, numbCells, numbBombs);
+        new Board(this, level, numbCells, numbBombs);
+    }
+
+    public void wonGame(boolean wontheGame) {
+
+        if (wontheGame) {
+            String textWinner = "YOU BEAT ME!";
+            String AnnouncementWinner = "You finished the game! :)";
+            JOptionPane.showMessageDialog(null, AnnouncementWinner, textWinner, JOptionPane.INFORMATION_MESSAGE);
+
+
+        }
     }
 
     public void finishedGame(boolean lostGame) {
 
-        if (lostGame){
+        if (lostGame) {
+
             String textLoser = "GAME OVER";
             String Announcement = "The game has finished! :(";
             JOptionPane.showMessageDialog(null, Announcement, textLoser, JOptionPane.INFORMATION_MESSAGE);
+
         }
     }
 }
